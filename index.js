a4c19ecd555e431a23a3e02b7f747014ade1da0b
Roman Antl <romcaantl@gmail.com>
2024-01-06 17:22:14 +0100

    Public folder, updates

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async(_, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(80, () => {
-    console.log("Server running on port 80");
+const port = process.env.NODE_ENV === 'development' ? 8080 : 80;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
